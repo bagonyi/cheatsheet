1113fa455bb9ed4f7d4b2eb50163e1c4e07a0f1d
David Bagonyi <david.bagonyi@gmail.com>
2019-03-02 23:41:28 +0000

    [getopts] Update getopts.sh to show using options with parameters

--- shell/getopts.sh
+++ shell/getopts.sh
@@ -8,11 +8,12 @@ function my-command () {
         my-command - Some description of the command.
 
     SYNOPSIS
-        my-command [-ho] ARGUMENT1 ARGUMENT2
+        my-command [-ho] [-p PARAMETER] ARGUMENT1 ARGUMENT2
 
     OPTIONS
         -h    Show this help.
-        -o    Some option.
+        -o    Some option without parameter.
+        -p    Some option with parameter.
 
     ARGUMENTS
         ARGUMENT1    Description of argument 1.
@@ -21,43 +22,54 @@ function my-command () {
     EXAMPLES
         # Some description of this example
         $ my-command hello world"
-        }
+    }
 
-        OPTION_SELECTED=false
+    local OPTION1_SELECTED=false
+    local OPTION2_SELECTED=false
+    local OPTION2_PARAMETER=
 
-        local OPTIND
-        while getopts ":ho" OPTION
-        do
-            case ${OPTION} in
-                h)
-                    print-usage
-                    exit
-                ;;
+    local OPTIND
+    while getopts ":hop:" OPTION
+    do
+        case ${OPTION} in
+            h)
+                print-usage
+                exit
+            ;;
 
-                o)
-                    OPTION_SELECTED=true
-                ;;
+            o)
+                OPTION1_SELECTED=true
+            ;;
 
-                \?)
-                    echo "Invalid option -$OPTARG"
-                    print-usage
-                    exit
-                ;;
+            p)
+                OPTION2_SELECTED=true
+                OPTION2_PARAMETER="${OPTARG}"
+            ;;
 
-                :)
-                    echo "Option -$OPTARG requires an argument"
-                    print-usage
-                    exit
-                ;;
-            esac
-        done
+            \?)
+                echo "Invalid option -$OPTARG"
+                print-usage
+                exit
+            ;;
 
-        shift $((OPTIND -1))
+            :)
+                echo "Option -$OPTARG requires an argument"
+                print-usage
+                exit
+            ;;
+        esac
+    done
 
-        ARGUMENT1=$1
-        ARGUMENT2=$2
+    shift $((OPTIND -1))
 
-        if [[ "${OPTION_SELECTED}" = true ]]; then
-            echo "Option -o was selected"
-        fi
+    ARGUMENT1=$1
+    ARGUMENT2=$2
+
+    if [[ "${OPTION1_SELECTED}" = true ]]; then
+        echo "Option -o was selected"
+    fi
+
+    if [[ "${OPTION2_SELECTED}" = true ]]; then
+        echo "Option -p was selected with parameter ${OPTION2_PARAMETER}"
+    fi
 }
